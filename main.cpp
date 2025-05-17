18ade313a917b0a3cd2019644074a980e5423efd
Santiago112506 <kperea848@uan.edu.co>
2025-05-17 16:36:01 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,69 +1,56 @@
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
+int main() 
 {
-#include <iostream>
+    
+    const int NUM_GENEROS = 5;
+    const int NUM_SUCURSALES = 3;
+    
+    string generos[NUM_GENEROS] = {"Ficción", "Ciencia", "Romance", "Terror", "Biografías"};
+    string sucursales[NUM_SUCURSALES] = {"Sucursal A", "Sucursal B", "Sucursal C"};
+    
+    
+    int libros_rentados[NUM_SUCURSALES][NUM_GENEROS] = {0};
+    
+    libros_rentados[0][0] = 10; 
+    libros_rentados[0][1] = 5;  
+    libros_rentados[1][2] = 8;  
+    libros_rentados[2][3] = 3;  
+    libros_rentados[2][4] = 7;  
+    
+    
+    cout << "REPORTE DE LIBROS RENTADOS\n";
+    
 
-struct GestionDeLibros {
-    char isbn[20];    // ISBN como cadena de caracteres
-    char titulo[100];  // Título del libro
-    float precio;
-    int cantidad;
-};
-
-Libro libros[100];    // Máximo 100 libros
-int totalLibros = 0;
-float caja = 1000000; // Inversión inicial
-
-void registrarLibro() {
-    if (totalLibros >= 100) {
-        std::cout << "¡No caben mas libros!\n";
-        return;
+    cout << "Sucursal\t";
+    for (int g = 0; g < NUM_GENEROS; g++) {
+        cout << generos[g] << "\t";
     }
+    cout << "Total\n";
+    
 
-    Libro l;
-    std::cout << "ISBN: ";
-    std::cin >> l.isbn;
-    std::cout << "Titulo: ";
-    std::cin.ignore();
-    std::cin.getline(l.titulo, 100);
-    std::cout << "Precio: ";
-    std::cin >> l.precio;
-    std::cout << "Cantidad: ";
-    std::cin >> l.cantidad;
-
-    libros[totalLibros] = l;
-    totalLibros++;
-    std::cout << "Libro registrado!\n";
-}
-
-void venderLibro() {
-    char isbn[20];
-    int cantidad;
-    std::cout << "ISBN del libro a vender: ";
-    std::cin >> isbn;
-    std::cout << "Cantidad: ";
-    std::cin >> cantidad;
-
-    for (int i = 0; i < totalLibros; i++) {
-        bool coincide = true;
-        for (int j = 0; isbn[j] != '\0'; j++) {
-            if (libros[i].isbn[j] != isbn[j]) {
-                coincide = false;
-                break;
-            }
+    for (int s = 0; s < NUM_SUCURSALES; s++) {
+        cout << sucursales[s] << "\t";
+        int total_sucursal = 0;
+        
+        for (int g = 0; g < NUM_GENEROS; g++) {
+            cout << libros_rentados[s][g] << "\t";
+            total_sucursal += libros_rentados[s][g];
         }
         
-        if (coincide) {
-            if (libros[i].cantidad >= cantidad) {
-                libros[i].cantidad -= cantidad;
-                caja += libros[i].precio * cantidad;
-                std::cout << "Venta realizada. Ganancia: $" 
-                          << libros[i].precio * cantidad << "\n";
-            } else {
-                std::cout << "¡No hay suficiente stock!\n";
-            }
-           
-
+        cout << total_sucursal << "\n";
+    }
+    
+    cout << "\nTotal por género:\t";
+    for (int g = 0; g < NUM_GENEROS; g++) {
+        int total_genero = 0;
+        for (int s = 0; s < NUM_SUCURSALES; s++) {
+            total_genero += libros_rentados[s][g];
+        }
+        cout << total_genero << "\t";
+    }
+    
     return 0;
 }
